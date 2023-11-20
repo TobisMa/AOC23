9596d361dafd3d358ab1e48f44ae32e891cccbec
TobisMa <tobias-ma@gmx.net>
2023-11-20 20:45:20 +0100

    Initial commit

---
+++ daySetup.py
@@ -0,0 +1,95 @@
+from datetime import datetime
+import os
+import sys
+import colorama
+from requests import Session
+colorama.Fore.CYAN
+CYAN = "\x1b[36m"
+RED = "\x1b[31m"
+YELLOW = "\x1b[33m"
+RESET = "\x1b[0m"
+
+AOC_BASE_URL = "https://adventofcode.com/%(year)s/day/%(day)s/input"
+
+date = datetime.utcnow()
+YEAR = date.year
+MONTH = date.month
+DAY = date.day
+
+print(f"Today: {DAY}.{MONTH}.{YEAR}")
+if MONTH != 12:
+    print(RED + "ERROR: Not december yet" + RESET)
+    raise SystemExit(1)
+
+DAY_REPR = str(DAY).zfill(2)
+FOLDER_NAME = "day" + DAY_REPR
+
+if not os.access(FOLDER_NAME,  os.R_OK | os.W_OK):
+    print(CYAN + "INFO: Creating folder %r" % FOLDER_NAME)
+    os.mkdir(FOLDER_NAME)
+else:
+    print(CYAN + "INFO: Folder alreay exists")
+    
+args = sys.argv[1:]
+filetype = ""   # filetype needs to be extension
+if "h" in args or "haskell" in args or "hs" in args:
+    filetype = "hs"
+else:
+    filetype = "py"
+    
+FILENAME = os.path.join(FOLDER_NAME, "day" + DAY_REPR + "." + filetype)
+DATA_FILE = os.path.join(FOLDER_NAME, "data_" + FOLDER_NAME.lower() + ".txt")
+if not os.access(FILENAME, os.R_OK | os.W_OK) or "-f" in args:
+    print(CYAN + "INFO: Created file for solution...")
+    has_templ = False
+    try:
+        with open(filetype + ".templ", "r") as tf:
+            template = tf.read().replace("$$FILE$$", DATA_FILE.replace("\\", "\\\\"))
+    except FileNotFoundError:
+        print(YELLOW + "WARNING: No template for file type %r found" % filetype)
+    else:
+        has_templ = True
+
+    with open(FILENAME, "w") as f:
+        if has_templ:
+            f.write(template)  # type: ignore
+
+elif "-f" not in args:
+    print(YELLOW + "WARNING: File already exists. Use -f override")
+    
+print(CYAN + "INFO: Fetching todays puzzle input...")
+day_url = AOC_BASE_URL % {"year": YEAR, "day": DAY}
+
+SESSION_FILE = ".session_id"
+try:
+    with open(SESSION_FILE) as sess_file:
+        sess_id =  sess_file.read().strip()
+        
+except FileNotFoundError:
+    print(RED + "ERROR: Could not read session id")
+    print(RED + "ERROR: Is the file %r present?" % SESSION_FILE)
+    print(RED + "ERROR: If creating, don't forget to put the file into your .gitignore" + RESET)
+    raise SystemExit(1)
+
+
+print(CYAN + "INFO: Trying to access URL %r..." % day_url)
+with Session() as s:
+    s.cookies.update({"session": sess_id})
+    response = s.get(day_url)
+    if not response.ok:
+        print(YELLOW + "WARNING: Server responded with %s" % response.status_code + RESET)
+        raise SystemExit(1)
+    
+    data = response.text
+
+print(CYAN + "INFO: Creating data file...")
+
+if not os.access(DATA_FILE, os.W_OK | os.R_OK) or "-d" in args:
+    with open(DATA_FILE, "w") as f:
+        f.write(data)
+
+elif not "-d" in args:
+    print(YELLOW + "WARNING: Data file already exists")
+    print("WARNING: Use -d to override")
+    
+print(CYAN + "INFO: Done" + RESET)
