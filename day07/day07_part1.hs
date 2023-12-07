cb5b528748bb4fc1177a104008d1c9a3de4d38fc
TobisMa <tobias-ma@gmx.net>
2023-12-07 20:31:45 +0100

    Day07 part1 and template change

--- day07/day07_part1.hs
+++ day07/day07_part1.hs
@@ -3,16 +3,70 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Char (ord, chr)
+import Data.List (sort, sortOn, groupBy)
 
 
 main :: IO()
 main = do
-    contents <- readFile "$$FILE$$"
+    contents <- readFile "day07\\data_day07.txt"
+    -- contents <- readFile "day07\\example.txt"
     putStrLn (solve contents)
 
 solve :: String -> String
-solve inp = 
+solve inp = show (sum (foldl (\acc x -> bidding mHands x acc) [] sorted))
+    where
+        hands = map (\[a, b] -> (a, strToint b)) (matFromString inp "\n" " ")
+        mHands = mapFromList hands
+        sortHands = sortOn (\(x, _) -> getType (count x)) hands
+        cs = groupBy (\(a, b) (c, d) -> getType (count a) == getType (count c)) sortHands
+        sorted = flatten (map (sortBy (\(a, _) (b, _) -> differ a b)) cs)
 
+bidding :: Map String Int -> (String, Int) -> [Int] -> [Int]
+bidding hands hand acc = ((hands ! fst hand) * (length acc + 1)) : acc
+
+len :: [a] -> Int
+len [] = 0
+len (_:xs) = 1 + len xs
+
+getType :: Map Char Int -> Int
+getType = rankCase . sort . Map.elems
+
+flatten :: [[a]] -> [a]
+flatten = concat
+
+cards :: Map Char Int
+cards = mapFromList [('A', 14), ('K', 13), ('Q', 12), ('J', 11), ('T', 10), ('9', 9), ('8', 8), ('7', 7), ('6', 6), ('5', 5), ('4', 4), ('3', 3), ('2', 2), ('1', 1)]
+
+highestCard :: String -> Int
+highestCard hand = maximum (map (cards !) hand)
+
+differ :: String -> String -> Bool
+differ [] [] = False
+differ (a:as) (b:bs)
+    | a == b = differ as bs
+    | otherwise = cards ! a > cards ! b
+
+sortBy :: Ord a => (a -> a -> Bool) -> [a] -> [a]
+sortBy _ [] = []
+sortBy f (x:lst) = left ++ [x] ++ right
+    where
+        left = sortBy f (filter (f x) lst)
+        right = sortBy f (filter (not . f x) lst)
+
+count :: String -> Map Char Int
+count = foldr (\x acc -> if Map.member x acc then Map.insert x ((acc ! x) + 1) acc else Map.insert x 1 acc) Map.empty
+-- cards :: Matrix String -> 
+
+rankCase :: [Int] -> Int
+rankCase [5] = 7        -- five pair
+rankCase [1, 4] = 6     -- four pair
+rankCase [2, 3] = 5     -- full house
+rankCase [1,1,3] = 4    -- three of a kind
+rankCase [1, 2, 2] = 3  -- two pair
+rankCase [1, 1, 1, 2] = 2  -- one pair
+rankCase [1, 1, 1, 1, 1] = 1  -- high card
+rankCase l = error ("For " ++ show l ++ "! Wtf")
 
 -- matrix
 type CharGrid = Matrix Char
@@ -46,8 +100,8 @@ gridEmpty = matEmpty
 matFromString :: String -> String -> String -> Matrix String
 matFromString str lineDel rowDel = map (`rowFromString` rowDel) (splitStr str lineDel "")
 
-matFromList :: [a] -> Matrix a
-matFromList lst = map rowFromList lst
+-- matFromList :: [a] -> Matrix a
+-- matFromList lst = map rowFromList lst
 
 rowFromString :: String -> String -> Row String
 rowFromString str del = splitStr str del ""
@@ -72,7 +126,7 @@ setInit = setFromList []
 mapInit :: Ord k => Map k v
 mapInit = mapFromList []
 
-mapFromList :: Ord k => [(k, v)] -> Map k v 
+mapFromList :: Ord k => [(k, v)] -> Map k v
 mapFromList lst = Map.fromList lst
 
 mapToList :: Ord k => Map k v -> [(k, v)]
@@ -89,7 +143,7 @@ removeDuplicatesRec [] = []
 removeDuplicatesRec (x:xs) = x : removeDuplicatesRec (filter (/= x) xs)
 
 
-getFileLines :: String -> [String] 
+getFileLines :: String -> [String]
 getFileLines = lines
 
 concatValuesByLine :: Show a => [a] -> String
@@ -101,7 +155,7 @@ concatStrsByLine = unlines
 concatValues :: Show a => [a] -> String
 concatValues = concatMap show
 
-concatStrs :: [String] -> String -> String 
+concatStrs :: [String] -> String -> String
 concatStrs (first:strs) sep = foldl (\acc x -> acc ++ sep ++ x) first strs
 
 valuesToStr :: Show a => [a] -> [String]
@@ -123,8 +177,8 @@ splitStr s del buf
         next = take (length del) s
         following = drop (length del) s
 
-strToint :: String -> Integer
-strToint s = read s :: Integer
+strToint :: String -> Int
+strToint s = read s :: Int
 
 strToFloat :: String -> Float
 strToFloat s = read s :: Float
