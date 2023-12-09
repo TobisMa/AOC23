96bb10b9bc4890202a5fd9f25798c770baeb32dc
TobisMa <tobias-ma@gmx.net>
2023-12-09 12:00:15 +0100

    Day09 that was way easier than other days

---
+++ day09/day09_part1.hs
@@ -0,0 +1,148 @@
+import Prelude hiding (splitAt)
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.Map (Map, (!))
+import qualified Data.Map as Map
+
+
+main :: IO()
+main = do
+    contents <- readFile "day09\\data_day09.txt"
+    -- contents <- readFile "day09\\example.txt"
+    putStrLn (solve contents)
+
+solve :: String -> String
+solve inp = show (sum (extrapolated))
+    where
+        history = matToList (matMap strToint (matFromString inp "\n" " "))
+        res = map diff history
+        extrapolated = map extrapolate res
+
+diff :: [Integer] -> [[Integer]]
+diff nums
+    | all (==0) nums = [nums]
+    | otherwise = nums: diff (foldr (\x acc -> nums !! x - nums !! (x-1) : acc) [] [1..length nums - 1])
+
+extrapolate :: [[Integer]] -> Integer
+extrapolate seqs = foldr (\x acc -> last x + acc) 0 (take (length seqs - 1) seqs)
+
+-- matrix
+type CharGrid = Matrix Char
+type Matrix a = [Row a]
+type Row a = [a]
+
+matToList :: Matrix a -> [[a]]
+matToList m = m
+
+matGet :: Matrix a -> Int -> Int -> a
+matGet m x y = (m !! y) !! x
+
+matSet :: Matrix a -> Int -> Int -> a -> Matrix a
+matSet m x y value = take y m ++ [rowSet (m !! y) x value] ++ drop (y+1) m
+
+matMap :: (a -> b) -> Matrix a -> Matrix b
+matMap f = map (map f)
+
+matRowMap :: (Row a -> Row b) -> Matrix a -> Matrix b
+matRowMap = map
+
+rowSet :: Row a -> Int -> a -> Row a
+rowSet r i v = take i r ++ [v] ++ drop (i+1) r
+
+charGridFromString :: String -> String -> CharGrid
+charGridFromString str lineDel = splitStr str lineDel ""
+
+matEmpty :: Int -> Int -> a -> Matrix a
+matEmpty rowCount lineCount fill = replicate lineCount (replicate rowCount fill)
+
+gridEmpty :: Int -> Int -> Char -> CharGrid
+gridEmpty = matEmpty
+
+matFromString :: String -> String -> String -> Matrix String
+matFromString str lineDel rowDel = map (`rowFromString` rowDel) (splitStr str lineDel "")
+
+matFromList :: [[a]] -> Matrix a
+matFromList lst = map rowFromList lst
+
+rowFromString :: String -> String -> Row String
+rowFromString str del = splitStr str del ""
+
+rowFromList :: [a] -> Row a
+rowFromList lst = lst
+
+showGrid :: CharGrid -> String
+showGrid g = concatStrs g "\n" ++ "\n"
+
+-- sets
+setFromList :: Ord a => [a] -> Set a
+setFromList = Set.fromList
+
+setToList :: Set a -> [a]
+setToList = Set.toList
+
+setInit :: Ord a => Set a
+setInit = setFromList []
+
+-- maps
+mapInit :: Ord k => Map k v
+mapInit = mapFromList []
+
+mapFromList :: Ord k => [(k, v)] -> Map k v
+mapFromList lst = Map.fromList lst
+
+mapToList :: Ord k => Map k v -> [(k, v)]
+mapToList m = Map.toList m
+
+
+
+-- other
+removeDuplicates :: Ord a => [a] -> [a]
+removeDuplicates lst = setToList (setFromList lst)
+
+removeDuplicatesRec :: Eq a => [a] -> [a]
+removeDuplicatesRec [] = []
+removeDuplicatesRec (x:xs) = x : removeDuplicatesRec (filter (/= x) xs)
+
+
+getFileLines :: String -> [String]
+getFileLines = lines
+
+concatValuesByLine :: Show a => [a] -> String
+concatValuesByLine v = unlines (map show v)
+
+concatStrsByLine :: [String] -> String
+concatStrsByLine = unlines
+
+concatValues :: Show a => [a] -> String
+concatValues = concatMap show
+
+concatStrs :: [String] -> String -> String
+concatStrs (first:strs) sep = foldl (\acc x -> acc ++ sep ++ x) first strs
+
+valuesToStr :: Show a => [a] -> [String]
+valuesToStr = map show
+
+-- keep splitStrAt because of already existing solutions. Remove for next year
+splitStrAt :: String -> Char -> String -> [String]
+splitStrAt "" _ buf = [buf | buf /= ""]
+splitStrAt (c:s) del buf
+    | c == del = buf : splitStrAt s del ""
+    | otherwise = splitStrAt s del (buf ++ [c])
+
+splitStr :: String -> String -> String -> [String]
+splitStr "" _ buf = [buf | buf /= ""]
+splitStr s del buf
+    | next == del = buf : splitStr following del ""
+    | otherwise = splitStr (tail s) del (buf ++ [head s])
+    where
+        next = take (length del) s
+        following = drop (length del) s
+
+strToint :: String -> Integer
+strToint s = read s :: Integer
+
+strToFloat :: String -> Float
+strToFloat s = read s :: Float
+
+strToDouble :: String -> Double
+strToDouble s = read s :: Double
